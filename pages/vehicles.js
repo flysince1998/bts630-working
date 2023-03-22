7a1b7a84a3d4de145d0bac5938a2575e67e23747
Eden Jeremiah <32580971+flysince1998@users.noreply.github.com>
2023-03-22 00:24:51 +0000

    Correcting errors

--- pages/vehicles.js
+++ pages/vehicles.js
@@ -64,9 +64,7 @@ export default function Vehicles() {
           </Card.Body>
         </Card>
         <br />
- {filteredData.length === 0 ? (
-  <p>No results found.</p>
-) : (
+ (
         <Table striped bordered>
           <thead>
             <tr>
@@ -92,7 +90,7 @@ export default function Vehicles() {
 
           </tbody>
         </Table>
-)}
+
       </>
     );
 }
