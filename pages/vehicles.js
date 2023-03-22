83f6d6602809046dffbde58888575656fb4f54cb
Eden Jeremiah <32580971+flysince1998@users.noreply.github.com>
2023-03-22 00:22:11 +0000

    Correcting errors

--- pages/vehicles.js
+++ pages/vehicles.js
@@ -29,7 +29,7 @@ export default function Vehicles() {
       const maxPrice = event.target.value;
       if(!isNaN(maxPrice))
       {
-        setMaxPrice(value);
+        setMaxPrice(maxPrice);
       }
     };
 
