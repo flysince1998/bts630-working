32937d1e51d5b9cd1529740f7404be5fba30d20e
Eden Jeremiah <32580971+flysince1998@users.noreply.github.com>
2023-03-22 01:30:21 +0000

    Trying to make the price filter work

--- pages/vehicles.js
+++ pages/vehicles.js
@@ -33,12 +33,10 @@ export default function Vehicles() {
     if (query && !(vehicle.make.toLowerCase().includes(query.toLowerCase()) || vehicle.model.toLowerCase().includes(query.toLowerCase()))) {
       return false;
     }
-  
-    if (minPrice !== '' && parseInt(vehicle.price) < parseInt(minPrice)) {
+    if (minPrice !== null && parseInt(vehicle.price) < parseInt(minPrice)) {
       return false;
     }
-  
-    if (maxPrice !== '' && parseInt(vehicle.price) > parseInt(maxPrice)) {
+    if (maxPrice !== null && parseInt(vehicle.price) > parseInt(maxPrice)) {
       return false;
     }
     
